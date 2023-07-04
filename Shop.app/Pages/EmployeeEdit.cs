810ba565d3c16954abb06a62255780e23b0924b9
baha mestiri <mohamedbaha.mestiri@esprit.tn>
2023-07-04 11:44:57 +0100

    working on dialog component ! ( add employee is not completed yet )

---
+++ Shop.app/Pages/EmployeeEdit.cs
@@ -0,0 +1,21 @@
+﻿using BethanysPieShopHRM.Shared;
+using Microsoft.AspNetCore.Components;
+using Shop.app.Services;
+
+namespace Shop.app.Pages
+{
+    public partial class EmployeeEdit
+    {
+        [Inject]
+        public EmployerDataService _EmployerDataService { get; set;  }
+        [Parameter]
+        public String EmployeeId { get; set; }
+        //the employee where we will be doing the data binding ! 
+        private  Employee Employee = new Employee();
+        protected override async Task OnInitializedAsync()
+        {
+            Employee = await _EmployerDataService.GetEmployeeDetails(int.Parse(EmployeeId)); 
+                base.OnInitializedAsync();
+        }
+    }
+}
